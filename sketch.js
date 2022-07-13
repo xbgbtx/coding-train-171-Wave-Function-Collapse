16b8f68edebb0343d0a0ea98fd37b8de027bbca7
Virthe <virthe@accursed-lands.net>
2022-07-13 11:26:00 +0100

    Add p5js

--- sketch.js
+++ sketch.js
@@ -1,1 +1,12 @@
-console.log("Hello!");
+function setup() {
+  createCanvas(800, 600);
+}
+
+function draw() {
+  if (mouseIsPressed) {
+    fill(0);
+  } else {
+    fill(255);
+  }
+  ellipse(mouseX, mouseY, 80, 80);
+}
