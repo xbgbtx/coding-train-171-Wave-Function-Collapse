2946f48bf37cc58fcdb98723d2c2d04761819057
Virthe <virthe@accursed-lands.net>
2022-07-16 11:29:41 +0100

    Display grid of tiles

--- sketch.js
+++ sketch.js
@@ -1,204 +1,56 @@
-const width = 720;
-const height = 720;
-const tileSize = 36;
-const tilesX = width / tileSize;
-const tilesY = height / tileSize;
+let tileData;
+let tileGrid;
 
-const tileData = new Set();
-let undecidedTile;
-
-let grid;
-
-const Directions = {
-  UP: 0,
-  RIGHT: 1,
-  DOWN: 2,
-  LEFT: 3,
-};
-
-const Tiles = {
-  UP: 0,
-  RIGHT: 1,
-  DOWN: 2,
-  LEFT: 3,
-  BLANK: 4,
-  UNDECIDED: 5,
+const tileTypes = {
+  UNDECIDED: 0,
 };
 
 class Tile {
-  constructor(imageRef, type, edgeVals) {
+  constructor(imageRef) {
     this.image = loadImage(imageRef);
-    this.type = type;
-    this.edgeVals = edgeVals;
-  }
-
-  validNeighbour(dir, tile) {
-    if (tile == undecidedTile) return true;
-
-    return this.validNeighbours[dir].has(tile);
   }
 }
 
-const neighbourhood = [
-  { x: 0, y: -1 },
-  { x: 1, y: 0 },
-  { x: 0, y: 1 },
-  { x: -1, y: 0 },
-];
+class TileGrid {
+  constructor(w, h) {
+    this.w = w;
+    this.h = h;
+    this.tiles = [];
 
-const invalidNeighbour = {};
-
-function indexFromCoords({ x, y }) {
-  return tilesX * y + x;
-}
-
-function coordsFromIndex(i) {
-  return {
-    x: i % tilesX,
-    y: floor(i / tilesX),
-  };
-}
-function validCoords({ x, y }) {
-  return x >= 0 && x < tilesX && y >= 0 && y < tilesY;
-}
-
-class Cell {
-  constructor() {
-    this.possibilities = new Set(tileData);
-    this.tile = undefined;
-  }
-
-  getTile() {
-    if (this.possibilities.size > 1) return undecidedTile;
-    return tile;
-  }
-
-  entropy() {
-    return this.possibilities.length;
-  }
-
-  removePossibility(p) {
-    this.possibilities = this.possibilities.filter((i) => i != p);
-  }
-
-  collapse() {
-    const c = random(this.possibilities);
-    this.possibilities = [c];
-  }
-}
-
-class WFCGrid {
-  constructor() {
-    this.cells = [];
-
-    for (let i = 0; i < tilesX * tilesY; i++) {
-      const cell = new Cell();
-      this.cells.push(cell);
+    for (let i = 0; i < w * h; i++) {
+      this.tiles.push(tileData.get(tileTypes.UNDECIDED));
     }
-
-    this.open = [floor(random(this.cells.length))];
   }
 
-  getNumCells() {
-    return this.cells.length;
+  tileSize() {
+    return { x: width / this.w, y: height / this.h };
   }
 
-  getCellAtIndex(i) {
-    return this.cells[i];
-  }
+  draw() {
+    for (let i = 0; i < this.tiles.length; i++) {
+      const row = floor(i / this.w);
+      const col = i % this.w;
+      const ts = this.tileSize();
+      const x = col * ts.x;
+      const y = row * ts.y;
+      const tImg = this.tiles[i].image;
 
-  removePossibility(index, p) {
-    let cell = this.cells[index];
-    cell.removePossibility(p);
-  }
-
-  getEntropyMap(cellIndexes) {
-    return cellIndexes.reduce((entropyMap, cellIdx) => {
-      const e = this.cells[cellIdx].entropy();
-
-      entropyMap.has(e)
-        ? entropyMap.get(e).push(cellIdx)
-        : entropyMap.set(e, [cellIdx]);
-
-      return entropyMap;
-    }, new Map());
-  }
-
-  collapseNext() {
-    if (this.open.length <= 0) return false;
-
-    const entropyMap = this.getEntropyMap(this.open);
-
-    const sortedEntropyKeys = Array.from(entropyMap.keys())
-      .filter((k) => k > 1)
-      .sort((k1, k2) => k1 - k2);
-
-    if (sortedEntropyKeys.size < 1) return false;
-
-    const lowestEntropy = sortedEntropyKeys[0];
-
-    if (lowestEntropy <= 1) return false;
-
-    const candidates = entropyMap.get(lowestEntropy);
-    const c = random(candidates);
-
-    const neighbours = this.neighbours(c);
-
-    this.cells[c].collapse(neighbours);
-    this.open = this.open.filter((i) => i !== c);
-
-    const nextOpen = neighbours.filter(
-      (x) => x !== invalidNeighbour && this.cells[x].entropy() > 1
-    );
-
-    this.open = this.open.concat(nextOpen);
-
-    return true;
-  }
-
-  neighbours(cellIdx) {
-    const cellCoords = coordsFromIndex(cellIdx);
-
-    const cellNeighbourhood = neighbourhood
-      .map((n) => ({
-        x: n.x + cellCoords.x,
-        y: n.y + cellCoords.y,
-      }))
-      .map((n) => (validCoords(n) ? indexFromCoords(n) : invalidNeighbour));
-
-    return cellNeighbourhood;
+      image(tImg, x, y, ts.x, ts.y);
+    }
   }
 }
 
 function preload() {
-  let undecidedTile = new Tile("tiles/undecided.png", Tiles.UNDECIDED, null);
-  tileData.add(new Tile("tiles/up.png", Tiles.UP, [1, 1, 0, 1]));
-  tileData.add(new Tile("tiles/right.png", Tiles.RIGHT, [1, 1, 1, 0]));
-  tileData.add(new Tile("tiles/down.png", Tiles.DOWN, [0, 1, 1, 1]));
-  tileData.add(new Tile("tiles/left.png", Tiles.LEFT, [1, 0, 1, 1]));
-  tileData.add(new Tile("tiles/blank.png", Tiles.BLANK, [0, 0, 0, 0]));
+  tileData = new Map();
+  tileData.set(tileTypes.UNDECIDED, new Tile("tiles/undecided.png"));
 }
 
 function setup() {
-  const title = "171 - Wave Function Collapse";
-  document.getElementById("sketch-title").innerText = title;
-  window.document.title = title;
-
   createCanvas(720, 720);
-  grid = new WFCGrid();
-  //grid.collapseNext();
+  tileGrid = new TileGrid(10, 10);
 }
 
 function draw() {
-  const status = grid.collapseNext();
-
-  for (let i = 0; i < grid.getNumCells(); i++) {
-    const t = grid.getCellAtIndex(i).getTile();
-    const cellCoords = coordsFromIndex(i);
-    const x = cellCoords.x * tileSize;
-    const y = cellCoords.y * tileSize;
-    image(t.image, x, y, tileSize, tileSize);
-  }
-
-  if (status == false) noLoop();
+  tileGrid.draw();
+  noLoop();
 }
