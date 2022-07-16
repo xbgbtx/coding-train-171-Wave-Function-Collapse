475e988713d61a15ca11ecd512be4bb4a6c72269
Virthe <virthe@accursed-lands.net>
2022-07-16 12:39:49 +0100

    Add WFC data structure

--- sketch.js
+++ sketch.js
@@ -1,5 +1,6 @@
 let tileData;
 let tileGrid;
+let wfc;
 
 const tileTypes = {
   UP: 0,
@@ -44,24 +45,59 @@ class TileGrid {
       image(tImg, x, y, ts.x, ts.y);
     }
   }
+
+  setTile(idx, tile) {
+    this.tiles[idx] = tile;
+  }
+}
+
+class WFC {
+  constructor(initialOpen, types, undecided) {
+    this.open = [initialOpen];
+    this.types = types;
+    this.undecided = undecided;
+  }
+
+  collapseNext() {
+    const next = this.open[floor(random(this.open.length))];
+
+    const newType = this.types[floor(random(this.types.length))];
+
+    const newTile = tileData.get(newType);
+
+    tileGrid.setTile(next, newTile);
+  }
 }
 
 function preload() {
   tileData = new Map();
   tileData.set(tileTypes.UP, new Tile("tiles/up.png"), [1, 1, 0, 1]);
   tileData.set(tileTypes.RIGHT, new Tile("tiles/right.png"), [1, 1, 1, 0]);
-  tileData.set(tileTypes.DOWN, new Tile("tiles/undecided.png"), [0, 1, 1, 1]);
-  tileData.set(tileTypes.LEFT, new Tile("tiles/undecided.png"), [1, 0, 1, 1]);
-  tileData.set(tileTypes.BLANK, new Tile("tiles/undecided.png"), [0, 0, 0, 0]);
+  tileData.set(tileTypes.DOWN, new Tile("tiles/down.png"), [0, 1, 1, 1]);
+  tileData.set(tileTypes.LEFT, new Tile("tiles/left.png"), [1, 0, 1, 1]);
+  tileData.set(tileTypes.BLANK, new Tile("tiles/blank.png"), [0, 0, 0, 0]);
   tileData.set(tileTypes.UNDECIDED, new Tile("tiles/undecided.png"), []);
 }
 
 function setup() {
   createCanvas(720, 720);
   tileGrid = new TileGrid(10, 10);
+  wfc = new WFC(
+    0,
+    [
+      tileTypes.UP,
+      tileTypes.RIGHT,
+      tileTypes.DOWN,
+      tileTypes.LEFT,
+      tileTypes.UP,
+      tileTypes.BLANK,
+    ],
+    tileTypes.UNDECIDED
+  );
 }
 
 function draw() {
+  wfc.collapseNext();
   tileGrid.draw();
   noLoop();
 }
