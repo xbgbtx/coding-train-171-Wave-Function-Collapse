2be0a18a03e99d8a3715b8392cb7610045094857
Virthe <virthe@accursed-lands.net>
2022-07-19 13:10:56 +0100

    Create GridBuffer and use it for draw

--- sketch.js
+++ sketch.js
@@ -11,26 +11,6 @@ const tileTypes = {
   UNDECIDED: 5,
 };
 
-const directions = {
-  UP: { x: 0, y: -1 },
-  RIGHT: { x: 1, y: 0 },
-  DOWN: { x: 0, y: 1 },
-  LEFT: { x: -1, y: 0 },
-};
-
-function oppositDirection(d) {
-  switch (d) {
-    case "UP":
-      return "DOWN";
-    case "RIGHT":
-      return "LEFT";
-    case "DOWN":
-      return "UP";
-    case "LEFT":
-      return "RIGHT";
-  }
-}
-
 class Tile {
   constructor(type, imageRef, edges) {
     this.type = type;
@@ -43,11 +23,7 @@ class TileGrid {
   constructor(w, h) {
     this.w = w;
     this.h = h;
-    this.tiles = [];
-
-    for (let i = 0; i < w * h; i++) {
-      this.tiles.push(tileData.get(tileTypes.UNDECIDED));
-    }
+    this.tiles = new GridBuffer(w, h, tileTypes.UNDECIDED);
   }
 
   tileSize() {
@@ -55,50 +31,17 @@ class TileGrid {
   }
 
   draw() {
-    for (let i = 0; i < this.tiles.length; i++) {
-      const row = floor(i / this.w);
-      const col = i % this.w;
+    for (let { cellValue, coords } of this.tiles) {
       const ts = this.tileSize();
-      const x = col * ts.x;
-      const y = row * ts.y;
-      const tImg = this.tiles[i].image;
-
+      const x = coords.x * ts.x;
+      const y = coords.y * ts.y;
+      const tImg = tileData.get(cellValue).image;
       image(tImg, x, y, ts.x, ts.y);
     }
   }
 
   setTile(idx, tile) {
     this.tiles[idx] = tile;
-  }
-
-  idxToCoord(idx) {
-    return { x: idx % this.w, y: floor(idx / this.w) };
-  }
-
-  coordToIdx({ x, y }) {
-    return y * this.w + x;
-  }
-
-  validCoord({ x, y }) {
-    return x >= 0 && x < this.w && y >= 0 && y < this.h;
-  }
-
-  getNeighbour(idx, { x, y }) {
-    const c = this.idxToCoord(idx);
-    const n = { x: c.x + x, y: c.y + y };
-
-    if (!this.validCoord(n)) return null;
-
-    return this.coordToIdx(n);
-  }
-
-  getAllNeighbours(idx) {
-    const neighbours = [];
-    for (const d in directions) {
-      const n = this.getNeighbour(idx, directions[d]);
-      neighbours.push(n);
-    }
-    return neighbours;
   }
 }
 
@@ -113,7 +56,7 @@ class WFC {
   collapseNext() {
     const next = this.popRandomOpen();
 
-    const neighbours = tileGrid.getAllNeighbours(next);
+    const neighbours = tileGrid.tiles.getAllNeighbours(next);
 
     const newType = this.collapse(next);
     const newTile = tileData.get(newType);
@@ -137,39 +80,7 @@ class WFC {
     return r;
   }
 
-  collapse(idx) {
-    const possibleTypes = new Set(this.types);
-
-    for (const t of possibleTypes) {
-      for (const d in directions) {
-        //edge value for this tile
-        const tEdge = tileData.get(t).edges[d];
-
-        const directionOffset = directions[d];
-        const neighbourIdx = tileGrid.getNeighbour(idx, directionOffset);
-
-        if (neighbourIdx === null) continue;
-
-        const neighbourType = tileGrid.tiles[neighbourIdx].type;
-
-        if (neighbourType == tileTypes.UNDECIDED) continue;
-
-        const neighbourOpp = oppositDirection(d);
-        const neighbourEdge = tileData.get(neighbourType).edges[neighbourOpp];
-
-        if (tEdge !== neighbourEdge) {
-          possibleTypes.delete(t);
-          break;
-        }
-      }
-    }
-
-    const arrayPoissbleTypes = Array.from(possibleTypes);
-
-    if (arrayPoissbleTypes.length == 0) return tileTypes.UNDECIDED;
-
-    return arrayPoissbleTypes[floor(random() * arrayPoissbleTypes.length)];
-  }
+  collapse(idx) {}
 }
 
 function preload() {
@@ -244,7 +155,7 @@ function setup() {
 }
 
 function draw() {
-  wfc.collapseNext();
+  //wfc.collapseNext();
   tileGrid.draw();
 
   if (wfc.open.length <= 0) noLoop();
